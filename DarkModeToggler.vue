2be880c3eefe5f457c12df14e4d54627e1f58d6c
Elias Moreira <56691312+emoreiravirtus@users.noreply.github.com>
2021-03-05 15:48:33 -0300

    Update DarkModeToggler.vue

--- DarkModeToggler.vue
+++ DarkModeToggler.vue
@@ -20,9 +20,11 @@ export default {
         const switchTheme = e => {
             if (e.target.checked) {
                 document.documentElement.setAttribute('data-theme', 'dark');
+                localStorage.setItem('theme', 'dark');
             }
             else {
                 document.documentElement.setAttribute('data-theme', 'light');
+                localStorage.setItem('theme', 'light');
             }    
         }
 
